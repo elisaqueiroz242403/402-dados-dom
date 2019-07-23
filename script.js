d504cecb1b43045318c65473aabdd54b54d95e98
karen queiroz <kqueiroz812@gmail.com>
2019-07-23 17:36:06 -0300

    adicionar os exemplos de js

---
+++ script.js
@@ -0,0 +1,59 @@
+// // // // // let compras = ['batata', 'beringela']
+// // // // // console.log(compras);
+// // // // // compras.push("cebola");
+// // // // // console.log("cebola");
+// // // // // compras.pop();
+// // // // // console.log();
+// // // // // compras.unshift("pepino");
+// // // // // compras.shift("pepino");
+
+
+
+// // // // function somar(num1, num2){
+// // // //     console.log(num1 + num2);
+// // // // }
+// // // // somar(10, 67);
+// // // // somar(234, 567);
+
+// // // // function dividir(num1, num2){
+// // // //     console.log(num1/ num2);
+// // // // }
+
+// // // // dividir(155, 5);
+// // // // dividir(67, 3);
+
+// // // // function multiplicar(num1, num2){
+// // // //    console.log(num1*num2); 
+// // // // }
+// // // // multiplicar(5, 3);
+ 
+
+
+// // // function somar(a,b){
+// // //     return a+b;
+// // // }
+// // // // arrow fuction
+// // // somar = (a,b) =>{
+// // //     return a+b
+// // // }
+
+// // console.log(document);
+
+// // D.O.M.
+
+
+// let corpo = document.querySelector("body");
+// let botoes = document.querySelector("button");
+
+
+
+// function trocarCor(){
+//     corpo.style.backgroundColor = "blue";
+// }
+
+// botao.onclick = trocarCor;
+
+
+
+
+
